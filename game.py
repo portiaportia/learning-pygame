b43663a8475207d8c32aa323cc4755103a76b87b
portiaportia <portiaplante@gmail.com>
2025-02-28 09:13:57 -0500

    beginning

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-print("hello")
+import pygame, sys
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Game Setup
+pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+pygame.display.set_caption("GAME TITLE")
+
+# Game Loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
